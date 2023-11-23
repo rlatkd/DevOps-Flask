77332731f493eb44192cd7a462b0049d461b5c48
rlatkdgns <tkdgns96@naver.com>
2023-11-24 08:46:06 +0900

    modified:   scripts/after_install.sh 	modified:   scripts/run_flask.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,7 +5,8 @@ cd   /home/ubuntu/ssgbay
 echo ">>> pip install ----------------------"
 pip install -r requirements.txt
 
-RUN crontab -l | { cat; echo "* * * * * /usr/local/bin/python /app/historyUpdate.py >> /var/log/cron.log 2>&1"; } | crontab -
+echo ">>> cron settings --------------------"
+crontab -l | { cat; echo "* * * * * /usr/bin/python3 /home/ubuntu/ssgbay/historyUpdate.py >> /var/log/cron.log 2>&1"; } | crontab -
 
 echo ">>> npm install ----------------------"
 npm install
@@ -16,5 +17,3 @@ rm -rf appspec.yml requirements.txt
 
 echo ">>> change owner to ubuntu -----------"
 chown -R ubuntu /home/ubuntu/ssgbay
-# echo ">>> run app --------------------------"
-# python3 -u app.py > /dev/null 2> /dev/null < /dev/null &
--- scripts/run_flask.sh
+++ scripts/run_flask.sh
@@ -4,5 +4,7 @@ cd  /home/ubuntu/ssgbay
 
 echo ">>> run app --------------------------"
 
+mkdir resources
+
 cron
 python3 -u app.py > /dev/null 2> /dev/null < /dev/null &
