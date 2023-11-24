72957f6d4882844b8d0305e8b41e627fe7212d11
rlatkdgns <tkdgns96@naver.com>
2023-11-24 09:48:17 +0900

    modified:   appspec.yml 	deleted:    resources/.placeholder 	new file:   scripts/afterInstall.sh 	deleted:    scripts/after_install.sh 	renamed:    scripts/before_install.sh -> scripts/beforeInstall.sh 	new file:   scripts/runServer.sh 	deleted:    scripts/run_flask.sh

--- scripts/runServer.sh
+++ scripts/runServer.sh
@@ -2,8 +2,7 @@
 
 cd  /home/ubuntu/ssgbay
 
-echo ">>> run app --------------------------"
-
+echo ">>> run app -------------------------------------------------------"
 
 cron
 python3 -u app.py > /dev/null 2> /dev/null < /dev/null &
